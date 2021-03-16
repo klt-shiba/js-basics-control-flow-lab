494a0120c89b456414c2b0b752dfc3b08d0755db
Colin Tseng <klt.aus@yahoo.com>
2021-03-16 22:13:35 +1100

    Completed message

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value < 400) {
+    return "This one is on me!"
+  }
+  else if ((value > 2000) && (value > 2500 )) {
+  
+      return "No can do."
+
+    } else {
+
+    return "I will gladly take your thirty bucks."
+
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city == "NYC") ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+    switch (tip) {
+      case "generous" : 
+      return "Thank you so much.";
+
+      case "not as generous" : 
+      return "Thank you.";
+
+      default:
+        return "Bye."
+    }
+
 }
+  // if (!(tip == 'generous') && (tip != 'generous')) {
+      
+  //     return "Bye."
+
+  // }  else if (tip != 'generous') {
+
+  //     return "Thank you." 
+    
+  // } else {
+
+  //   return "Thank you so much." 
+
+  //   }
